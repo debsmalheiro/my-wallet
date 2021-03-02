189a01b831632d5f80086fb4ab85444f058211d1
Déborah Malheiro <deblima_@hotmail.com>
2021-03-02 10:34:53 -0300

    criando header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,14 +1,27 @@
 // Dependencies
-import React from 'react';
+import React, {useMemo} from 'react';
 
 // Style
-import { Container } from './styles';
+import { Container, Profile, Welcome, UserName } from './styles';
+
+// Emojis
+import emojis from '../../utils/emojis';
 
 // Component
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice];
+    }, [])
+
     return (
         <Container>
-            MainHeader
+            <h1>Toggle</h1>
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName> Déborah Malheiro</UserName>
+            </Profile>
         </Container>
     )
 }
