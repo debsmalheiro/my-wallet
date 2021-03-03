daa6ed8310fac047ac86b3c7e593e12d61fdd971
Déborah Malheiro <deblima_@hotmail.com>
2021-03-03 16:24:12 -0300

    feat: layout dos filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Styles
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 // Components
 import ContentHeader from "../../components/ContentHeader";
@@ -11,17 +11,48 @@ import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 // Component
 const List: React.FC = () => {
-  const options = [
-    { value: "Jolie", label: "Jolie" },
-    { value: "Moana", label: "Moana" },
-    { value: "Maui", label: "Maui" },
+  const months = [
+    { value: 1, label: "Janeiro" },
+    { value: 2, label: "Fevereiro" },
+    { value: 3, label: "Março" },
+    { value: 4, label: "Abril" },
+    { value: 5, label: "Maio" },
+    { value: 6, label: "Junho" },
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+    { value: 10, label: "Outubro" },
+    { value: 11, label: "Novembro" },
+    { value: 12, label: "Dezembro" },
+  ];
+
+  const years = [
+    { value: 2021, label: 2021 },
+    { value: 2020, label: 2020 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor="#E44C4F">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button 
+          type="button"
+          className="tag-filter tag-filter-recurrent"
+        >
+          Recorrentes
+        </button>
+        <button 
+          type="button"
+          className="tag-filter tag-filter-eventual"
+        >
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
         <HistoryFinanceCard
           tagColor="#E44C4E"
@@ -29,7 +60,12 @@ const List: React.FC = () => {
           subtitle="03/03/2021"
           amount="R$ 130,00"
         />
-        
+        <HistoryFinanceCard
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="03/03/2021"
+          amount="R$ 130,00"
+        />
       </Content>
     </Container>
   );
