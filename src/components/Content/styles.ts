fadccbc8e31f7a24fba9e190d929b80b3e450ded
Déborah Malheiro <deblima_@hotmail.com>
2021-03-03 14:25:44 -0300

    personalizando scroll e fixando aside e o header

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -7,4 +7,26 @@ export const Container = styled.div`
     background-color: ${props => props.theme.colors.primary};
 
     padding: 25px;
+
+    /* Barra de rolagem */
+
+    /* Só pode aparecer 100% descontando os 70px do cabeçalho */
+    height: calc(100vh - 70px);
+    /* Para não mostrar o que passa da área do conteúdo */
+    overflow-y: scroll; 
+
+    /* Estilizando o scroll bar */
+
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.secondary};
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: ${props => props.theme.colors.tertiary};
+    }
 `;
