99b940800eded9efd0525b4fb8f0b70ee61dfa80
Déborah Malheiro <deblima_@hotmail.com>
2021-03-05 14:48:17 -0300

    refactor: identificaçao de cores do tipo de gasto

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -34,7 +34,7 @@ export const Filters = styled.div`
         display: block;
         width: 55px;
         margin: 0 auto;
-        border-bottom: 10px solid ${props => props.theme.colors.warning};
+        border-bottom: 10px solid ${props => props.theme.colors.success};
     }
 
     .tag-filter-eventual::after {
@@ -42,6 +42,6 @@ export const Filters = styled.div`
         display: block;
         width: 55px;
         margin: 0 auto;
-        border-bottom: 10px solid ${props => props.theme.colors.success};
+        border-bottom: 10px solid ${props => props.theme.colors.warning};
     }
 `;
