753e3ad84943e18e78c8a4c9d2b242ac876bfd1d
Déborah Malheiro <deblima_@hotmail.com>
2021-03-15 11:11:01 -0300

    feat: animacao no grafico de barras

--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,10 +1,25 @@
 // Dependencies
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 // Interface
 interface ILegendProps {
     color: string;
 }
+
+// Component
+const animate = keyframes`
+    0% {
+        transform: translateX(100px);
+        opacity: 0;
+    }
+    50% {
+        opacity: 0.3;
+    }
+    100% {
+        transform: translateX(0px);
+        opacity: 1;
+    }
+`;
 
 export const Container = styled.div`
     width: 48%;
@@ -14,6 +29,8 @@ export const Container = styled.div`
     color: ${props => props.theme.colors.white};
     border-radius: 7px;
     display: flex;
+
+    animation: ${animate} 0.5s;
 
     @media(max-width: 1200px) {
         display: flex;
