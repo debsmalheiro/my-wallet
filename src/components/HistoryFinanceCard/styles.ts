c9737a286536939da79a523a4ad59a4a0b5b5f84
Déborah Malheiro <deblima_@hotmail.com>
2021-03-15 11:13:22 -0300

    feat: animacao nas listas

--- src/components/HistoryFinanceCard/styles.ts
+++ src/components/HistoryFinanceCard/styles.ts
@@ -1,13 +1,26 @@
 // Dependencies
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 // Interface
-
 interface ITagProps {
     color: string;
 }
 
 // Component
+const animate = keyframes`
+    0% {
+        transform: translateX(-100px);
+        opacity: 0;
+    }
+    50% {
+        opacity: 0.3;
+    }
+    100% {
+        transform: translateX(0px);
+        opacity: 1;
+    }
+`;
+
 export const Container = styled.li`
     background-color: ${props => props.theme.colors.tertiary};
 
@@ -25,6 +38,8 @@ export const Container = styled.li`
     transition: all 0.3s;
 
     position: relative;
+
+    animation: ${animate} 0.5s ease;
 
     &:hover {
         opacity: 0.7;
